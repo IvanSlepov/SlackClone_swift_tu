7906721c9110738ff451e8ad363eafa922a500d6
Ivan Sliepov <slepow1985@gmail.com>
2020-09-12 22:20:30 +0300

    Initial Commit

---
+++ SlackClone/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SlackClone
+//
+//  Created by Ivan Sliepov on 12.09.2020.
+//  Copyright © 2020 Ivan Sliepov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SlackClone/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SlackClone
+//
+//  Created by Ivan Sliepov on 12.09.2020.
+//  Copyright © 2020 Ivan Sliepov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
